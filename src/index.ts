c7aa983d62f23210efb31b782ea3ca75e42c55fa
PedroSales117 <pedrohenrysalles@outlook.com>
2024-08-13 11:00:39 -0300

    feat(cli): add multilingual support and enhanced user prompts in project initializer
    - Implemented multilingual support for English and Portuguese.
    - Enhanced user prompts with input validation and cancellation options.
    - Refactored error messages and logging to support multiple languages.

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,158 @@ import inquirer from 'inquirer';
 import * as fs from 'fs';
 import { URL } from 'url';
 
-/**
- * Class to handle the initialization of a new project.
- */
+// Define the valid keys for messages
+type MessageKey =
+    | 'selectLanguage'
+    | 'projectName'
+    | 'repoURL'
+    | 'newRepoURL'
+    | 'operationCanceled'
+    | 'creatingProject'
+    | 'installingDependencies'
+    | 'settingRemote'
+    | 'projectReady'
+    | 'errorPackageJson'
+    | 'errorOccurred'
+    | 'unknownErrorOccurred'
+    | 'invalidGitRepoURL';
+
+// Interface for the structure of messages in each language
+interface ILanguageMessages {
+    selectLanguage: string;
+    projectName: string;
+    repoURL: string;
+    newRepoURL: string;
+    operationCanceled: string;
+    creatingProject: string;
+    installingDependencies: string;
+    settingRemote: string;
+    projectReady: string;
+    errorPackageJson: string;
+    errorOccurred: string;
+    unknownErrorOccurred: string;
+    invalidGitRepoURL: string;
+}
+
+// Interface for the object containing all messages
+interface IMessages {
+    en: ILanguageMessages;
+    pt: ILanguageMessages;
+}
+
+class Messages {
+    static messages: IMessages = {
+        en: {
+            selectLanguage: 'Select your language',
+            projectName: 'What is your project name? (c: to cancel)',
+            repoURL: 'Which TypeScript template repository you want to use (Repo URL)? (c: to cancel)',
+            newRepoURL: 'Enter the new GitHub repository URL for your project (leave blank to skip, c: to cancel):',
+            operationCanceled: 'Operation canceled by the user.',
+            creatingProject: 'Creating a new project in .',
+            installingDependencies: 'Installing dependencies 💼...',
+            settingRemote: 'Setting new remote origin to ',
+            projectReady: 'Project is ready to go! 🚀🚀🚀',
+            errorPackageJson: 'Error: package.json not found in the template. Please check the template structure.',
+            errorOccurred: 'Failed to create project due to an error: ',
+            unknownErrorOccurred: 'An unknown error occurred: ',
+            invalidGitRepoURL: 'Please enter a valid GitHub repository URL.',
+        },
+        pt: {
+            selectLanguage: 'Selecione seu idioma',
+            projectName: 'Qual é o nome do seu projeto? (c: para cancelar)',
+            repoURL: 'Qual repositório de template TypeScript você deseja usar (URL do repositório)? (c: para cancelar)',
+            newRepoURL: 'Insira a nova URL do repositório GitHub para o seu projeto (deixe em branco para pular, c: para cancelar):',
+            operationCanceled: 'Operação cancelada pelo usuário.',
+            creatingProject: 'Criando um novo projeto em .',
+            installingDependencies: 'Instalando dependências 💼...',
+            settingRemote: 'Configurando novo remote origin para ',
+            projectReady: 'Projeto está pronto para começar! 🚀🚀🚀',
+            errorPackageJson: 'Erro: package.json não encontrado no template. Verifique a estrutura do template.',
+            errorOccurred: 'Falha ao criar o projeto devido a um erro: ',
+            unknownErrorOccurred: 'Ocorreu um erro desconhecido: ',
+            invalidGitRepoURL: 'Por favor, insira uma URL válida do repositório GitHub.',
+        }
+    };
+
+    static currentLanguage: keyof IMessages = 'en';
+
+    /**
+     * Sets the current language for messages.
+     * @param {keyof IMessages} language - The language to set (e.g., 'en' or 'pt').
+     */
+    static setLanguage(language: keyof IMessages) {
+        this.currentLanguage = language;
+    }
+
+    /**
+     * Retrieves a message based on the current language and key.
+     * @param {MessageKey} key - The key of the message to retrieve.
+     * @returns {string} - The localized message.
+     */
+    static get(key: MessageKey): string {
+        return this.messages[this.currentLanguage][key];
+    }
+}
+
 class ProjectInitializer {
+
+    /**
+     * Prompts the user for the language, and updates the prompt message based on the language selected.
+     */
+    private static async promptLanguage(): Promise<void> {
+        const { language } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'language',
+                message: Messages.get('selectLanguage'),
+                choices: [
+                    {
+                        name: 'English',
+                        value: 'en',
+                        short: 'English',
+                    },
+                    {
+                        name: 'Português',
+                        value: 'pt',
+                        short: 'Português',
+                    }
+                ],
+                filter: function (val) {
+                    Messages.setLanguage(val);
+                    return val;
+                },
+                transformer: function (input: string, flags: { isFinal: boolean; }) {
+                    if (flags.isFinal) {
+                        return Messages.get('selectLanguage');
+                    }
+                    return input === 'en' ? 'Select your language' : 'Selecione seu idioma';
+                }
+            }
+        ]);
+
+        Messages.setLanguage(language);
+    }
 
     /**
      * Validates if the given project name contains only letters, numbers, underscores, and dashes.
      * @param {string} name - The project name to validate.
-     * @returns {boolean} - Returns true if valid, false otherwise.
+     * @returns {boolean|string} - Returns true if valid, otherwise returns an error message.
      */
-    private static isValidProjectName(name: string): boolean {
-        return /^[a-zA-Z0-9_-]+$/.test(name);
+    private static isValidProjectName(name: string): boolean | string {
+        return /^[a-zA-Z0-9_-]+$/.test(name) || Messages.get('projectName');
     }
 
     /**
      * Validates if the given URL is a valid GitHub repository URL.
      * @param {string} url - The URL to validate.
-     * @returns {boolean} - Returns true if valid, false otherwise.
+     * @returns {boolean|string} - Returns true if valid, otherwise returns an error message.
      */
-    private static isValidGitRepoURL(url: string): boolean {
+    private static isValidGitRepoURL(url: string): boolean | string {
         try {
             const parsedUrl = new URL(url);
-            return parsedUrl.origin === 'https://github.com';
+            return parsedUrl.origin === 'https://github.com' || Messages.get('invalidGitRepoURL');
         } catch (error) {
-            return false;
+            return Messages.get('invalidGitRepoURL');
         }
     }
 
@@ -37,88 +164,47 @@ class ProjectInitializer {
      * Prompts the user for inputs, including the option to cancel.
      */
     private static async promptUser() {
+
+        await this.promptLanguage();
+
         const answers = await inquirer.prompt([
             {
-                type: 'list',
+                type: 'input',
                 name: 'projectName',
-                message: 'What is your project name?',
-                choices: [
-                    {
-                        name: 'Enter project name',
-                        value: 'input',
-                    },
-                    {
-                        name: 'Cancel',
-                        value: 'cancel',
-                    },
-                ],
-                validate: (input: string) => this.isValidProjectName(input) || 'Project name can only contain letters, numbers, underscores, and dashes.',
-                filter: async (val: string) => {
-                    if (val === 'cancel') {
-                        console.log('Operation canceled by the user.');
+                message: Messages.get('projectName'),
+                validate: (input: string) => {
+                    if (input.toLowerCase() === 'c') {
+                        console.log(); // Forces a line break
+                        console.log(Messages.get('operationCanceled'));
                         process.exit(0);
                     }
-                    return await inquirer.prompt({
-                        type: 'input',
-                        name: 'projectName',
-                        message: 'Please enter your project name:',
-                        validate: (input: string) => this.isValidProjectName(input) || 'Project name can only contain letters, numbers, underscores, and dashes.',
-                    }).then(answer => answer.projectName);
+                    return this.isValidProjectName(input);
                 },
             },
             {
-                type: 'list',
+                type: 'input',
                 name: 'repoURL',
-                message: 'Which TypeScript template repository you want to use (Repo URL)?',
-                choices: [
-                    {
-                        name: 'Enter repository URL',
-                        value: 'input',
-                    },
-                    {
-                        name: 'Cancel',
-                        value: 'cancel',
-                    },
-                ],
-                validate: (input: string) => this.isValidGitRepoURL(input) || 'Please enter a valid GitHub repository URL.',
-                filter: async (val: string) => {
-                    if (val === 'cancel') {
-                        console.log('Operation canceled by the user.');
+                message: Messages.get('repoURL'),
+                validate: (input: string) => {
+                    if (input.toLowerCase() === 'c') {
+                        console.log(); // Forces a line break
+                        console.log(Messages.get('operationCanceled'));
                         process.exit(0);
                     }
-                    return await inquirer.prompt({
-                        type: 'input',
-                        name: 'repoURL',
-                        message: 'Please enter the repository URL:',
-                        validate: (input: string) => this.isValidGitRepoURL(input) || 'Please enter a valid GitHub repository URL.',
-                    }).then(answer => answer.repoURL);
+                    return this.isValidGitRepoURL(input);
                 },
             },
             {
-                type: 'list',
+                type: 'input',
                 name: 'newRepoURL',
-                message: 'Enter the new GitHub repository URL for your project (leave blank to skip):',
-                choices: [
-                    {
-                        name: 'Enter new repository URL',
-                        value: 'input',
-                    },
-                    {
-                        name: 'Cancel',
-                        value: 'cancel',
-                    },
-                ],
-                filter: async (val: string) => {
-                    if (val === 'cancel') {
-                        console.log('Operation canceled by the user.');
+                message: Messages.get('newRepoURL'),
+                validate: (input: string) => {
+                    if (input.toLowerCase() === 'c') {
+                        console.log(); // Forces a line break
+                        console.log(Messages.get('operationCanceled'));
                         process.exit(0);
                     }
-                    return await inquirer.prompt({
-                        type: 'input',
-                        name: 'newRepoURL',
-                        message: 'Please enter the new repository URL (leave blank to skip):',
-                        validate: (input: string) => input === '' || this.isValidGitRepoURL(input) || 'Please enter a valid GitHub repository URL.',
-                    }).then(answer => answer.newRepoURL);
+                    return input === '' || this.isValidGitRepoURL(input);
                 },
             }
         ]);
@@ -134,7 +220,7 @@ class ProjectInitializer {
         const { projectName, repoURL, newRepoURL } = await this.promptUser();
 
         try {
-            console.log(`Creating a new project in ./${projectName} 👷‍♀️🚧`);
+            console.log(`${Messages.get('creatingProject')} ./${projectName} 👷‍♀️🚧`);
             execSync(`git clone ${repoURL} ${projectName}`, { stdio: 'inherit' });
 
             const packagePath = `${projectName}/package.json`;
@@ -143,24 +229,24 @@ class ProjectInitializer {
                 packageFile.name = projectName;
                 fs.writeFileSync(packagePath, JSON.stringify(packageFile, null, 2), 'utf8');
 
-                console.log('Installing dependencies 💼...');
+                console.log(Messages.get('installingDependencies'));
                 execSync(`cd ${projectName} && npm install`, { stdio: 'inherit' });
 
                 // If the user provided a new repository URL, update the remote origin
                 if (newRepoURL) {
-                    console.log(`Setting new remote origin to ${newRepoURL} 🔧`);
+                    console.log(`${Messages.get('settingRemote')} ${newRepoURL} 🔧`);
                     execSync(`cd ${projectName} && git remote set-url origin ${newRepoURL}`, { stdio: 'inherit' });
                 }
 
-                console.log('Project is ready to go! 🚀🚀🚀');
+                console.log(Messages.get('projectReady'));
             } else {
-                console.error('Error: package.json not found in the template. Please check the template structure.');
+                console.error(Messages.get('errorPackageJson'));
             }
         } catch (error) {
             if (error instanceof Error) {
-                console.error(`Failed to create project due to an error: ${error.message}`);
+                console.error(`${Messages.get('errorOccurred')}${error.message}`);
             } else {
-                console.error(`An unknown error occurred: ${error}`);
+                console.error(`${Messages.get('unknownErrorOccurred')}${error}`);
             }
         }
     }
@@ -169,8 +255,8 @@ class ProjectInitializer {
 // Execute the project initialization
 ProjectInitializer.createProject().catch((err) => {
     if (err instanceof Error) {
-        console.error(`Encountered an error: ${err.message}`);
+        console.error(`${Messages.get('errorOccurred')}${err.message}`);
     } else {
-        console.error('An unknown error occurred during the project initialization.');
+        console.error(Messages.get('unknownErrorOccurred'));
     }
 });
